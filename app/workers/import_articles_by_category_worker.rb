9396fd0896366d0606a54a2e9ca83a51b7ce6dbe
Sean <sean.xiaoxu.ye@gmail.com>
2019-12-29 22:01:10 -0500

    added sidekiq worker to import articles by category

---
+++ app/workers/import_articles_by_category_worker.rb
@@ -0,0 +1,7 @@
+class ImportArticlesByCategoryWorker
+  include Sidekiq::Worker
+
+  def perform(category_name:)
+    Articles::Operations::ImportArticlesByCategory.call(category_name: category_name)
+  end
+end
