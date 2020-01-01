a3fc1495cd659aeb729f2f2b941ea8de71f2eef4
Sean <sean.xiaoxu.ye@gmail.com>
2020-01-01 16:30:26 -0500

    added logic that if there is an error, the rest of the actions would be skipped, and next operation will start

--- app/concepts/articles/actions/get_story_data.rb
+++ app/concepts/articles/actions/get_story_data.rb
@@ -5,14 +5,19 @@ module Articles
     promises :hackernews_story_data, :nokogiri_story_data
 
     executed do |context|
-      story_id = context.story_id
+      begin
+        story_id = context.story_id
 
-      hackernews_story_data =
-        Utils::ApiRequests.get_story_data(story_id: story_id)
-      nokogiri_story_data =
-        Nokogiri.HTML(
-          open("#{hackernews_story_data.dig('url')}", 'User-Agent' => 'firefox')
-        )
+        hackernews_story_data =
+          Utils::ApiRequests.get_story_data(story_id: story_id)
+        nokogiri_story_data =
+          Nokogiri.HTML(
+            open("#{hackernews_story_data.dig('url')}", 'User-Agent' => 'firefox')
+          )
+      rescue StandardError => e
+        puts "Rescued: #{e.inspect}"
+        context.skip_all!('Failed to retrieve story data')
+      end
 
       if hackernews_story_data.blank? && nokogiri_story_data.blank?
         context.fail_and_return!('Failed to retrieve story data')
