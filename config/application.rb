ddedcb5580fb4faf78348547920235aa0e88a0a5
Sean <sean.xiaoxu.ye@gmail.com>
2020-01-01 14:51:24 -0500

    added sidekiq to application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Awesomeapp
     config.action_controller.forgery_protection_origin_check = false
 
     config.autoload_paths += %W[#{config.root}/app/concepts]
+
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
