b7e4d88f9fddce2384c68618a01b05a9f1c1cdf0
Sean <sean.xiaoxu.ye@gmail.com>
2020-01-01 14:09:33 -0500

    added sidekiq admin panel route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'static/index'
   root 'static#index'
   get '/check.txt', to: proc { [200, {}, %w[it_works]] }
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
