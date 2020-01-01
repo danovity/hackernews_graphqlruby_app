1976492b09f9b79595e9d18c2af7ee181bd6f259
Sean <sean.xiaoxu.ye@gmail.com>
2020-01-01 15:42:24 -0500

    changed url in sidekiq.rb, removed namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,11 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "#{ENV['REDIS_URL']}/#{ENV['REDIS_SIDEKIQ_DB']}",
-    namespace: "awesomeapp"
+    url: "redis://localhost:6379/0"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "#{ENV['REDIS_URL']}/#{ENV['REDIS_SIDEKIQ_DB']}",
-    namespace: "awesomeapp"
+    url: "redis://localhost:6379/0"
   }
 end
