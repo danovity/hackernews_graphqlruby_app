c699aee5a85e8b41198f7441bce0546ccd082f53
Sean <sean.xiaoxu.ye@gmail.com>
2020-01-27 16:44:21 -0500

    changed environment to production

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,6 +20,6 @@
 # Learn more: http://github.com/javan/whenever
 
 every 2.minutes do
-  rake "hackernews_reloaded:delete_all_articles"
-  rake "hackernews_reloaded:import_articles_from_all_categories"
+  rake "hackernews_reloaded:delete_all_articles", :environment => 'production'
+  rake "hackernews_reloaded:import_articles_from_all_categories", :environment => 'production'
 end
