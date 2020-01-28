8777ebfb0d75584c1d3e5659d06c809c4e06d996
Sean <sean.xiaoxu.ye@gmail.com>
2020-01-27 19:50:50 -0500

    changed rake tasks to run every 24 hours

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -7,7 +7,7 @@ load File.join( Rails.root, 'lib', 'tasks', 'hackernews_reloaded.rake')
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '35s' do
+scheduler.every '24h' do
   Rake::Task['hackernews_reloaded:delete_all_articles'].invoke
   Rake::Task['hackernews_reloaded:import_articles_from_all_categories'].invoke
 end
