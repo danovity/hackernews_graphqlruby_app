88834200179daf6d899a17278f7d68e0f7d4ad79
Sean <sean.xiaoxu.ye@gmail.com>
2020-01-27 19:22:36 -0500

    fixed typo

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -8,5 +8,5 @@ scheduler = Rufus::Scheduler.new
 
 scheduler.every '35s' do
   Rake::Task['hackernews_reloaded:delete_all_articles'].invoke
-  Rake::Task['hackernews_reloaded:import_articles_from_all_categories'].invok
+  Rake::Task['hackernews_reloaded:import_articles_from_all_categories'].invoke
 end
